0430496793802c698e6d61b2c97862fe4e0536d9
yukkodesu <17264509+yukkodesu@users.noreply.github.com>
2023-06-21 14:15:31 +0800

    feat: print welcome message

--- src/editor.rs
+++ src/editor.rs
@@ -1,5 +1,7 @@
 use crate::Terminal;
-use std::io::stdout;
+use std::{fmt::format, io::stdout};
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 use crossterm::{
     event::{KeyCode, KeyEvent, KeyModifiers},
@@ -18,6 +20,7 @@ impl Editor {
             terminal: Terminal::new().expect("Failed to initialize terminal"),
         }
     }
+
     pub fn run(&mut self) {
         loop {
             if let Err(e) = self.refresh_screen() {
@@ -32,6 +35,7 @@ impl Editor {
             Terminal::cursor_show();
         }
     }
+
     fn process_key(&mut self) -> Result<(), std::io::Error> {
         let key_event = Terminal::read_key()?;
         // execute!(stdout(), Print(format!("{:?}\r\n", key_event)))?;
@@ -47,13 +51,28 @@ impl Editor {
         }
         Ok(())
     }
+
+    fn draw_welcome_msg(&self) {
+        let welcome_msg = format!("Hecto editor -- version {}", VERSION);
+        let width = (self.terminal.size().width as usize).min(welcome_msg.len());
+        let pad = (self.terminal.size().width as usize - width) / 2;
+        let welcome_msg = format!("~{}{}\r\n", " ".repeat(pad), &welcome_msg[..width]);
+        queue!(stdout(), Print(&welcome_msg)).unwrap();
+    }
+
     fn draw_rows(&self) {
-        for _ in 0..self.terminal.size().height - 1 {
+        let height = self.terminal.size().height;
+        for row in 0..height - 1 {
             Terminal::clear_current_line();
-            queue!(stdout(), Print("~\r\n")).unwrap();
+            if row == height / 3 {
+                self.draw_welcome_msg();
+            } else {
+                queue!(stdout(), Print("~\r\n")).unwrap();
+            }
         }
         Terminal::flush();
     }
+
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::cursor_hide();
         Terminal::move_cursor(0, 0);
